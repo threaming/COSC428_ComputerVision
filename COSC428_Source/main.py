eb8e475ed138e54aa07b92bb5618c2f8b4f4fd2f
Andreas Ming <ami162@uclive.ac.nz>
2024-04-26 15:42:30 +1200

    finished reconstruction and detection algorithm
    created util and main file, refactoring for better readability. Removed large files. better directory

--- COSC428_Source/main.py
+++ COSC428_Source/main.py
@@ -1,18 +1,24 @@
 import pycolmap
 from pathlib import Path
+import open3d as o3d
+from PIL import Image
+import numpy as np
+import utils.utils as utils
 
-output_path = Path('data/reconstruction')
-image_dir = Path('data/images')
+# ---- settings ----
+reconstruct = False     # timeintensive!
+reconst_path = Path('data/reconstruction')
+pcloud_path = Path('data/pclouds')
+image_dir = Path('/media/ami162/ESD-USB/Exp1_smaller_lowflight')
+#pcloud_name = "pcd_lowflight"
+#pcloud_name = "pcd_complete"
+pcloud_name = "pcd_pix4d"
 
-output_path.mkdir()
-mvs_path = output_path / "mvs"
-database_path = output_path / "database.db"
+# ---- programm ----
+if reconstruct:
+    utils.compute_pcloud(image_dir, reconst_path)   
+pcloud = utils.convert_pcloud(reconst_path/"mvs"/"sparse", pcloud_path, pcloud_name)
+pcloud, _ = utils.clean_pcloud(pcloud, flipz=True)
+plants_bb, _ = utils.compute_plants(pcloud)
 
-pycolmap.extract_features(database_path, image_dir)
-pycolmap.match_exhaustive(database_path)
-maps = pycolmap.incremental_mapping(database_path, image_dir, output_path)
-maps[0].write(output_path, format='.txt')
-# dense reconstruction
-pycolmap.undistort_images(mvs_path, output_path, image_dir)
-#pycolmap.patch_match_stereo(mvs_path)  # requires compilation with CUDA
-pycolmap.stereo_fusion(mvs_path / "dense.ply", mvs_path)
+o3d.visualization.draw_geometries([pcloud]+ plants_bb, window_name=f'Pointcloud ({pcloud_name}) w/ Plant Bounding Boxes')
---
+++ COSC428_Source/workflow.md
@@ -0,0 +1,6 @@
+# Workflow of Colmaps
+
+1. Feature extraction
+2. Exhaustive feature matching
+3. Retriangulation
+4. Global bundle adjustment (gradient descent!)
